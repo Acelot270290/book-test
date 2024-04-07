be1c4cb6467f1045fc01e849d69021aeef6e0121
Alan Diniz <alan@splitpaybrasil.com.br>
2024-04-07 17:51:50 -0300

    first commit

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'contato@alandiniz.com.br',
+            'password' => Hash::make('123456789'),
+        ]);
+    }
+}
